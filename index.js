149967c354c75337a4009e5fe99d7af700d46230
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-03 10:47:28 -0500

    working on rating component

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 //Main endpoint for movies
 let endpoint = `https://green-subdued-atlasaurus.glitch.me/movies`
 
@@ -42,13 +44,13 @@ const getMovies = async () => {
 const updateMovie = async (id,movie) => {
     try{
         let updateRequest = await fetch(endpoint + `/${id}`,
-        {
-            method: 'PUT',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(movie)
-        })
+            {
+                method: 'PUT',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify(movie)
+            })
 
         return updateRequest
     }catch(err){
@@ -56,7 +58,7 @@ const updateMovie = async (id,movie) => {
     }
 }
 
-//Delete a moview
+//Delete a movie
 const deleteMovie = async (id) => {
     try{
         let deleteRequest = await fetch(endpoint + `/${id}`,{
@@ -70,60 +72,85 @@ const deleteMovie = async (id) => {
 }
 
 
-//Populate the moviews div
+//Populate the movies div
 const displayMovies = () => {
     console.log("I am running")
     const container = document.getElementById('movies')
 
-    //Clear the movies container
+//Clear the movies container
     container.innerHTML = ''
-    
-    //Iterate through all movies
-    allMovies.forEach(movie=>{ 
 
-        let cardBody = document.createElement('div')
-        
+//Iterate through all movies
+    allMovies.forEach(movie=>{
 
-        cardBody.className = "card-body"
-
-        //Create the movie image
+//Create the movie image
         let img = document.createElement('img')
-        img.src = movie.image
+        img.src = movie['image']
 
-        img.className = "card-img-top"
-
-        //Create the movie title
+//Create the movie title
         let title = document.createElement('h3')
-        title.innerText = movie.title
+        title.innerText = movie['title']
 
-        title.className = "card-title"
+//Create the movie rating
+// let rating = document.createElement('h4')
+// rating.innerText = movie['rating']
+        let ratingComponent = pizzaRatingComponent(movie,(rating)=>{
+            console.log("You clicked " + rating)
+        })
 
-        //Create the movie rating
-        let rating = document.createElement('h4')
-        rating.innerText = movie.rating
-
-        //Description
+//Description
         let description = document.createElement('p')
-        description.innerText = movie.description
+        description.innerText = movie['description']
 
-        description.className = "card-text"
-
-        //Create the movie card div
+//Create the movie card div
         let movieCard = document.createElement('div')
-        
-        //Add a class for css
-        movieCard.className = 'movie-card card'
-         
-        //Put  all the sauce together
-        cardBody.appendChild(img)
-        cardBody.appendChild(title)
-        cardBody.appendChild(rating)
-        cardBody.appendChild(description)
-        movieCard.appendChild(cardBody)
 
-        //Let it be like the beatles.
+//Add a class for css
+        movieCard.className = 'movie-card'
+
+//Put  all the sauce together
+        movieCard.appendChild(img)
+        movieCard.appendChild(title)
+        movieCard.appendChild(ratingComponent)
+        movieCard.appendChild(description)
+
+//Let it be like the beatles.
         container.appendChild(movieCard)
     })
+}
+
+
+//Pizza rating component
+const pizzaRatingComponent = (movie,onRatingChange) => {
+//Make the main container that will hold the pizzas for rating
+    let ratingContainer = document.createElement('div')
+
+    for(let i = 1; i <= 5; i++){
+        if(movie['rating'] < i){
+//Draw filled icon
+            let singlePizzaIcon = document.createElement('li')
+            singlePizzaIcon.className = 'fa-sharp fa-solid fa-pizza-slice'
+            singlePizzaIcon.setAttribute('rating',i)
+            singlePizzaIcon.onclick = (e) => {
+                let r = e.target.getAttribute('rating')
+                console.log(r)
+                onRatingChange(r)
+            }
+            ratingContainer.appendChild(singlePizzaIcon)
+        }else{
+//Draw opaque icon
+            let singlePizzaIcon = document.createElement('li')
+            singlePizzaIcon.className = 'fa-duotone fa-pizza-slice'
+            singlePizzaIcon.setAttribute('rating',i)
+            singlePizzaIcon.onclick = (e) => {
+                let r = e.target.getAttribute('rating')
+                console.log(r)
+                onRatingChange(r)
+            }
+            ratingContainer.appendChild(singlePizzaIcon)
+        }
+    }
+    return ratingContainer
 }
 
 (async ()=>{
